9e1a9a5fd86de8e4499f18c202c1462cbc8bfcca
swaraj choudhary <schoudhary4147@gmail.com>
2021-02-16 22:41:33 +0530

    Add files via upload

---
+++ includes_admin/header.php
@@ -0,0 +1,20 @@
+<div class="brand clearfix">
+	<a class="navbar-brand" href="home.php" style="font-size: 20px; padding-top:1%; color:#fff"><i class="fa fa-renren"></i>TSF Bank</a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+	<!--<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.png" class="ts-avatar hidden-side" alt=""> ACCOUNT <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">CHANGE PASSWORD</a></li>
+					<li><a href="logout.php">LOGOUT</a></li>
+				</ul>
+			</li>
+		</ul>--> 
+	</div>
+
+
+
+
+
+  
---
+++ includes_admin/leftbar.php
@@ -0,0 +1,24 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i>ADMIN DASHBOARD</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i>EVENT LIST</a>
+<ul>
+<li><a href="add-bloodgroup.php">ADD EVENT</a></li>
+<li><a href="manage-bloodgroup.php">MANAGE EVENT LIST</a></li>
+</ul>
+</li>
+
+
+				<li><a href="add-donor.php"><i class="fa fa-edit"></i>ADD EVENT MANAGER</a></li>
+				<li><a href="donor-list.php"><i class="fa fa-users"></i>EVENT MANAGER LIST</a></li>
+
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i>EVENTS QUERY</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> MANAGE PAGES</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> UPDATE CONTACT INFO</a></li>
+
+
+			</ul>
+		</nav>
